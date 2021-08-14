55fad815aaa85b55c37c25b09f621a14ad958f3b
GiggleSeagull <bogdan.200070@gmail.com>
2021-08-14 16:42:45 +0300

    Add iPhone

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ const swiper = new Swiper('.swiper-container', {
         clickable: true
       },
   });
+
+// document.getElementById('myScroll').addEventListener('click', scrollToElement);
+// function scrollToElement(e) {
+// element = document.getElementById("myElementToScroll")
+// element.scrollIntoView(true);
+// }
+
+$('[href^="#"]').on('click', function(){
+    let href = $(this).attr('href'), elem = $(document).find(href);
+    if(elem.length > 0) {
+      let posY = elem.eq(0).offset().top;
+      $('html, body').animate({
+        scrollTop: posY
+      }, 1000);
+    }
+    return false;
+  });
